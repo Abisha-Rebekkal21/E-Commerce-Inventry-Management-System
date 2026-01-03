160a61fef97efc66c66d75d03190ba467e0b6a5c
Abisha_Rebekkal <abisharebekkalt.23it@kongu.edu>
2026-01-03 07:54:39 +0530

    LinkedList

--- InventoryData.java
+++ InventoryData.java
@@ -3,9 +3,10 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList
 public class InventoryData {
     // Shared inventory list
-    public static ArrayList<String[]> inventory = new ArrayList<>();
+    public static List<String[]> inventory = new LinkedList<>();
 }
 
